0e7b7faf13aad6bd0638ebf4aa50da07ed8277af
Mooshaah <mohamedelshaararwy@gmail.com>
2025-09-04 17:54:37 +0300

    added splash screen

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,9 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
+  flutter_localization
   permission_handler_windows
+  speech_to_text_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
